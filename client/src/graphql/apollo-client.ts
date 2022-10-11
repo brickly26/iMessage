45a9854687b64e0f67b2ba58eb1cacbe2e854847
brickly26 <sabburak26@gmail.com>
2022-10-11 19:42:08 -0400

    added apollo client and provider

--- client/src/graphql/apollo-client.ts
+++ client/src/graphql/apollo-client.ts
@@ -1,5 +1,11 @@
 import { ApolloClient, HttpLink, HttpLink, InMemoryCache } from '@apollo/client'
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000/graphql'
+  uri: 'http://localhost:4000/graphql',
+  credentials: 'include',
+});
+
+export const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
 })
