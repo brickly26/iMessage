65622a4c6a72656053cd1562a9beba6116ee07ef
brickly26 <sabburak26@gmail.com>
2024-02-07 18:28:23 -0500

    reset

--- server/src/graphql/resolvers/index.ts
+++
@@ -1,7 +0,0 @@
-import userResolvers from "./user";
-import conversationsResolvers from "./conversations";
-import merge from "lodash.merge";
-
-const resolvers = merge({}, userResolvers, conversationsResolvers);
-
-export default resolvers;
