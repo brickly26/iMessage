1afd181218d532a00c166296a16cf77b69298182
brickly26 <sabburak26@gmail.com>
2024-02-24 11:38:36 -0500

    set up apollo front end client and apollo server to work with websockets

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,27 +10,53 @@ import typeDefs from "./graphql/typeDefs";
 import resolvers from "./graphql/resolvers";
 import { getSession } from "next-auth/react";
 import * as dotenv from "dotenv";
-import { GraphQLContext, Session } from "./util/types";
+import { GraphQLContext, Session, SubscriptionContext } from "./util/types";
 import { PrismaClient } from "@prisma/client";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/lib/use/ws";
+import { PubSub } from "graphql-subscriptions";
 
 async function main() {
   dotenv.config();
   const app = express();
   const httpServer = http.createServer(app);
 
+  // Create our WebSocket server using the HTTP server we just set up.
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: "/graphql/subscriptions",
+  });
+
   const schema = makeExecutableSchema({
     typeDefs,
     resolvers,
   });
 
+  // Context Parameters
+  const prisma = new PrismaClient();
+  const pubsub = new PubSub();
+
+  // Save the returned server's info so we can shutdown this server later
+  const serverCleanup = useServer(
+    {
+      schema,
+      context: async (ctx: SubscriptionContext): Promise<GraphQLContext> => {
+        if (ctx.connectionParams && ctx.connectionParams.session) {
+          const { session } = ctx.connectionParams;
+
+          return { session, prisma, pubsub };
+        }
+
+        return { session: null, prisma, pubsub };
+      },
+    },
+    wsServer
+  );
+
   const corsOptions = {
     origin: process.env.CLIENT_ORIGIN,
     credentials: true,
   };
-
-  // Context Parameters
-  const prisma = new PrismaClient();
-  // const pubsub
 
   const server = new ApolloServer({
     schema,
@@ -54,13 +80,26 @@ async function main() {
         return {
           session,
           prisma,
+          pubsub,
         };
       } else {
-        return { session: null, prisma };
+        return { session: null, prisma, pubsub };
       }
     },
     plugins: [
+      // Proper shutdown for the HTTP server.
       ApolloServerPluginDrainHttpServer({ httpServer }),
+
+      // Proper shutdown for the WebSocket server.
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
       ApolloServerPluginLandingPageLocalDefault({ embed: true }),
     ],
   });
--- server/src/util/types.ts
+++ server/src/util/types.ts
@@ -4,21 +4,33 @@ import {
   conversationPopulated,
   participantPopulated,
 } from "../graphql/resolvers/conversations";
+import { Context } from "graphql-ws/lib/server";
+import { PubSub } from "graphql-subscriptions";
+
+/**
+ * Server Configuration
+ */
 
 export interface GraphQLContext {
   session: Session | null;
   prisma: PrismaClient;
-  // pubsub
+  pubsub: PubSub;
 }
 
-/**
- * Users
- */
+export interface SubscriptionContext extends Context {
+  connectionParams: {
+    session?: Session;
+  };
+}
 
 export interface Session {
   user?: User;
   expires: ISODateString;
 }
+
+/**
+ * Users
+ */
 
 export interface User {
   id: string;
