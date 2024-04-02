166c5666cd8ef78b6b9336d37faec650ccf18291
brickly26 <sabburak26@gmail.com>
2024-04-02 15:45:43 -0400

    more testing

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,30 +5,30 @@ const nextConfig = {
   env: {
     APOLLO_GRAPHQL_SERVER_BASE_URL: process.env.APOLLO_GRAPHQL_SERVER_BASE_URL,
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://imessage.up.railway.app",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: "/api/:path*",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         {
+  //           key: "Access-Control-Allow-Origin",
+  //           value: "https://imessage.up.railway.app",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Methods",
+  //           value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value:
+  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
